5896190c8f28deffce9c7ee9b993f05d3f1aa69c
johanneshilmer <johanneshilmerson94@gmail.com>
2025-03-10 12:23:10 +0100

    Start

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-
-  const handleCount = () => {
-    setCount(count + 1);
-  };
   return (
-    <>
-      <button onClick={handleCount}>click</button>
-      <h1>{count}</h1>
-    </>
+    <div className="container">
+      <h1>Hello Music Quizer!</h1>
+    </div>
   );
 }
 
