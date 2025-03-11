7cf9276614feb1c63a5afda958c5ecf4414ddd62
johanneshilmer <johanneshilmerson94@gmail.com>
2025-03-11 11:44:00 +0100

    Update

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -22,7 +22,7 @@ const renderOptions = (option: Options) => {
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
+      <h1>Home page!!</h1>
       <ItemList items={options} renderItem={renderOptions} />
     </div>
   );
