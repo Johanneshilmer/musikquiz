fcca1fdcc2681d50cc27a1315080e058a2cc9a83
johanneshilmer <johanneshilmerson94@gmail.com>
2025-03-12 12:40:16 +0100

    update next.js

---
+++ my-app/src/app/components/ItemList.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function ItemList() {
+  return (
+    <div className="container Item-Lists">
+      <div className="Item-List">
+        <span>Pop</span>
+      </div>
+      <div className="Item-List">
+        <span>Rock</span>
+      </div>
+      <div className="Item-List">
+        <span>Rap</span>
+      </div>
+      <div className="Item-List">
+        <span>Techno</span>
+      </div>
+      <div className="Item-List">
+        <span>Country</span>
+      </div>
+      <div className="Item-List">
+        <span>Random</span>
+      </div>
+    </div>
+  );
+}
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,12 @@
+import ItemList from "./components/ItemList";
+
+export default function Home() {
+  return (
+    <>
+      <div className="container main-header">
+        <h1>Hello</h1>
+      </div>
+      <ItemList />
+    </>
+  );
+}
