f2fab3c685ee3c81a51e677425691e2359daf2b9
johanneshilmer <johanneshilmerson94@gmail.com>
2025-03-14 16:07:28 +0100

    test

--- main/src/app/page.tsx
+++ main/src/app/page.tsx
@@ -1,4 +1,6 @@
-export default function Home() {
+export default async function Home() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+  const data = await response.json();
   return (
     <>
       <div className="container main-header">
@@ -6,6 +8,7 @@ export default function Home() {
       </div>
       <div className="container">
         <h1>Here i will store the list</h1>
+        <h1>{data.name}</h1>
       </div>
     </>
   );
