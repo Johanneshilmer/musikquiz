b9a114519936bdae22d7c64c5479bf8a28d9f594
johanneshilmer <johanneshilmerson94@gmail.com>
2025-03-14 10:40:35 +0100

    // Content: Came on

---
+++ main/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <div className="container main-header">
+        <h1>Hello</h1>
+      </div>
+      <div className="container">
+        <h1>Here i will store the list</h1>
+      </div>
+    </>
+  );
+}
--- my-app/src/app/pages/api/auth/login.ts
+++
@@ -1,8 +0,0 @@
-
-export default function handler(req, res) {
-  const scope = "user-read-private user-read-email"; // Lägg till fler scopes vid behov
-
-  const authUrl = `${process.env.SPOTIFY_AUTH_URL}?response_type=code&client_id=${process.env.SPOTIFY_CLIENT_ID}&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(process.env.SPOTIFY_REDIRECT_URI)}`;
-
-  res.redirect(authUrl);
-}
